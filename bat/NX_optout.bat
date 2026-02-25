81092642ede3ec4fb171e3680e40583c9d48b110
G414B4D <108630779+G414B4D@users.noreply.github.com>
2026-02-25 07:11:22 -0700

    Fix error checking for NX setting change
    Signed-off-by: G414B4D <108630779+G414B4D@users.noreply.github.com>

--- bat/NX_optout.bat
+++ bat/NX_optout.bat
@@ -2,7 +2,7 @@ REM This is something that should be run during initial setup for everything Win
 
 @echo off
 bcdedit /set nx optout
-if %error% equ 0 (
+if %errorlevel% equ 0 (
     echo NX setting changed successfully to optout
 ) else (
     echo Failed. Be sure to run as an Administrator
